54300193b85df87f12af8392d3a97c6a5a5ebe2b
Ivan Hudzinskyi <ivanhudzinskyi2000@gmail.com>
2019-10-02 13:50:51 +0300

    Added properties
    Signed-off-by: Ivan Hudzinskyi <ivanhudzinskyi2000@gmail.com>

--- SysPro_Lab_02/AudioRecordingStudio.cs
+++ SysPro_Lab_02/AudioRecordingStudio.cs
@@ -8,5 +8,22 @@ namespace SysPro_Lab_02
 {
     class AudioRecordingStudio
     {
+        // properties
+        public string Name { get; }
+        public string Adress { get; }
+
+        public int NumberOfWorkers { get; private set; }
+
+        public float TrackRecordCost { get; private set; }
+        public int TrackRecordDuration { get; private set; }
+
+        public float WorkerWage { get; private set; }
+        public float TotalWorkersWage { get { return WorkerWage * NumberOfWorkers; } }
+
+        public float availableMoney { get; private set; }
+
+        public int NumberOfInstruments { get; private set; }
+
+        public int NumberOfRooms { get; private set; }
     }
 }
