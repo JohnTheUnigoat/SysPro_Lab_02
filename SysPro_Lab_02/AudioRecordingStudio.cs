3f67d0c78f9530fbca9eab7fc2c59f662b314b29
Ivan Hudzinskyi <ivanhudzinskyi2000@gmail.com>
2019-10-02 15:01:48 +0300

    Added room managing, some tweaks
    Signed-off-by: Ivan Hudzinskyi <ivanhudzinskyi2000@gmail.com>

--- SysPro_Lab_02/AudioRecordingStudio.cs
+++ SysPro_Lab_02/AudioRecordingStudio.cs
@@ -8,11 +8,13 @@ namespace SysPro_Lab_02
 {
     class AudioRecordingStudio
     {
-        // static fields
+        // static fields //
+
         static float instrumentCost = 750.0f;
         static float roomCost = 5500.0f;
 
-        // properties
+        // properties //
+
         public string Name { get; }
         public string Adress { get; }
 
@@ -30,6 +32,8 @@ namespace SysPro_Lab_02
 
         public int NumberOfRooms { get; private set; }
 
+        // methods //
+
         // constructor
         public AudioRecordingStudio(string name, string adress, float initialMoney)
         {
@@ -38,6 +42,7 @@ namespace SysPro_Lab_02
             AvailableMoney = initialMoney;
         }
 
+        // workers
         public void HireWorker()
         {
             NumberOfWorkers++;
@@ -48,19 +53,58 @@ namespace SysPro_Lab_02
             NumberOfWorkers--;
         }
 
-        public bool BuyInstrument()
+        // instruments
+        public bool BuyInstruments(int count = 1)
         {
-            if (AvailableMoney < instrumentCost)
+            float totalCost = instrumentCost * count;
+            if (AvailableMoney < totalCost)
                 return false;
 
-            AvailableMoney -= instrumentCost;
-            NumberOfInstruments++;
+            AvailableMoney -= totalCost;
+            NumberOfInstruments += count;
             return true;
         }
 
         public void ThrowAwayInstrument()
         {
             NumberOfInstruments--;
+        }
+
+        // rooms
+        public bool AddRoom()
+        {
+            int instrumentsMissing = NumberOfRooms * 2 - NumberOfInstruments;
+
+            float totalCost = roomCost + instrumentCost * instrumentsMissing;
+
+            if (AvailableMoney < totalCost)
+                return false;
+
+            AvailableMoney -= totalCost;
+            NumberOfRooms++;
+            NumberOfInstruments += instrumentsMissing;
+
+            return true;
+        }
+
+        public void DemolishRoom()
+        {
+            if (NumberOfRooms == 0)
+                throw new OperationCanceledException("No more rooms to demolish!");
+
+            NumberOfRooms--;
+        }
+
+        public static AudioRecordingStudio operator ++(AudioRecordingStudio a)
+        {
+            a.AddRoom();
+            return a;
+        }
+
+        public static AudioRecordingStudio operator --(AudioRecordingStudio a)
+        {
+            a.DemolishRoom();
+            return a;
         }
     }
 }
