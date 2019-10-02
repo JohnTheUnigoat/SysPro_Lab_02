5b32a8f4df19bf2c42ebb5f710be86b3824436b4
Ivan Hudzinskyi <ivanhudzinskyi2000@gmail.com>
2019-10-02 14:10:13 +0300

    Added constructor, worker and instrument managing
    Signed-off-by: Ivan Hudzinskyi <ivanhudzinskyi2000@gmail.com>

--- SysPro_Lab_02/AudioRecordingStudio.cs
+++ SysPro_Lab_02/AudioRecordingStudio.cs
@@ -8,6 +8,10 @@ namespace SysPro_Lab_02
 {
     class AudioRecordingStudio
     {
+        // static fields
+        static float instrumentCost = 750.0f;
+        static float roomCost = 5500.0f;
+
         // properties
         public string Name { get; }
         public string Adress { get; }
@@ -20,10 +24,43 @@ namespace SysPro_Lab_02
         public float WorkerWage { get; private set; }
         public float TotalWorkersWage { get { return WorkerWage * NumberOfWorkers; } }
 
-        public float availableMoney { get; private set; }
+        public float AvailableMoney { get; private set; }
 
         public int NumberOfInstruments { get; private set; }
 
         public int NumberOfRooms { get; private set; }
+
+        // constructor
+        public AudioRecordingStudio(string name, string adress, float initialMoney)
+        {
+            Name = name;
+            Adress = adress;
+            AvailableMoney = initialMoney;
+        }
+
+        public void HireWorker()
+        {
+            NumberOfWorkers++;
+        }
+
+        public void FireWorker()
+        {
+            NumberOfWorkers--;
+        }
+
+        public bool BuyInstrument()
+        {
+            if (AvailableMoney < instrumentCost)
+                return false;
+
+            AvailableMoney -= instrumentCost;
+            NumberOfInstruments++;
+            return true;
+        }
+
+        public void ThrowAwayInstrument()
+        {
+            NumberOfInstruments--;
+        }
     }
 }
